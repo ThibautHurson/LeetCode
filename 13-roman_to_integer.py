0788d6e6a93ae42324c5d5b620030f3259cb5b24
ThibautHurson <thibaut.hurson@supelec.fr>
2021-10-09 15:47:34 +0100

    a bit simplified

--- 13-roman_to_integer.py
+++ 13-roman_to_integer.py
@@ -1,22 +1,17 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        
-        numeral = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        exception = {'IV': 4,'IX': 9,'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
+        trans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
+                'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
+        i = 0
         res = 0
-        k = 0
-        n = len(s)
-        while k < n - 1:
-            if s[k:k+2] in exception:
-                res += exception[s[k:k+2]]
-                k += 2
+        while i < len(s):  
+            if i < len(s) - 1 and s[i: i+2] in trans:
+                res += trans[s[i: i+2]]
+                i += 2
             else:
-                res += numeral[s[k]]
-                k += 1
-        if k == n - 1: res += numeral[s[k]]
-            
+                res += trans[s[i]]
+                i += 1
         return res
-
 '''
 temp: O(n)
 spat: O(1)
